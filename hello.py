1eab1d29547a0929c961f64af0332c0f12047efd
philothink <ydaeyong@naver.com>
2022-05-06 13:22:15 +0900

    15배수 과제 215배수 과제 215배수 과제 215배수 과제 215배수 과제 215배수 과제 215배수 과제 215배수 과제 215배수 과제 215배수 과제 215배수 과제 215배수 과제 215배수 과제 215배수 과제 215배수 과제 2

--- hello.py
+++ hello.py
@@ -2,8 +2,14 @@
 
 
 
-for i in range(1,11):
-    if i %3==0:
+for i in range(1,21):
+    if i %15==0:
+        print("yeardream")
+
+    elif i%5 == 0:
+        print("dream")
+
+    elif i%3 == 0:
         print("year")
 
     else:
